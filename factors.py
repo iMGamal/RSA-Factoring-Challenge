09ae70c3e5fa924d67f60372bbc5f99bbc23e024
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-06 22:46:42 +0000

    test

---
+++ factors.py
@@ -0,0 +1,9 @@
+def factorize(n):
+    q = 2
+    if n % q == 0:
+        p = n / q
+    elif n % q != 0:
+        q += 1
+        p = n / q
+        print(str(n) + "=" + str(int(p)) + "*" + str(int(q)))
+        return ''
