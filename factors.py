846c37ab01d0203182f7d88b3bd1f5fe03a20f65
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-07 06:25:59 +0000

    yolo

---
+++ factors.py
@@ -0,0 +1,10 @@
+def factors(n):
+    q = int(2)
+    while q < n:
+        if n % q == 0:
+            p = int (n / q)
+            print(str(n) + "=" + str(p) + "*" + str(q))
+    return''
+with open("tests/test00", "r") as f:
+    n = print(f.read())
+print(factors(18))
